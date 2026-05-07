7637178d63a1e1098ad3f7dff06c3db7cff18317
Connor Gregorius <connor.gregorius@gmail.com>
2026-05-07 13:15:19 -0500

    minor fix

--- app/db.py
+++ app/db.py
@@ -23,7 +23,7 @@ def get_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="admin", #password for mysql database, change it to your own passowrd
+        password="YOUR MYSQL PASSWORD", #password for mysql database, change it to your own passowrd
         database="Library_Model" #database name for login information
     )
 
